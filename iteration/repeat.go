8e7cf9698346f8f3881f47f240d5ef71c398418b
Miguel Espinoza <miguel.espinozaleon@gmail.com>
2024-01-11 15:59:24 -0700

    first commit

---
+++ iteration/repeat.go
@@ -0,0 +1,11 @@
+package iteration
+
+
+
+func Repeat(character string, repeats int) string {
+	var repeated string
+	for i := 0; i < repeats; i++ {
+		repeated += character
+	}
+	return repeated
+}
